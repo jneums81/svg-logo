d5e5494525958f550ee477a76b686e950d273e5f
Jackson Neuman <jacksonneuman@Jacksons-Air-4.attlocal.net>
2023-10-18 17:14:46 -0500

    updated index.js file andadded a svg file

--- index.js
+++ index.js
@@ -2,26 +2,39 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const { Triangle, Circle, Square } = require('./lib/shapes');
 
+function validateText(input) {
+    return input.length <= 3 ? true : 'Text must be up to three characters.';
+  }
+
+// Function to validate color input (either keyword or hex code)
+function validateColor(input) {
+  // Add validation logic for color input (keyword or hex code)
+  const validKeyword = /^([a-zA-Z]+)$/; // Example: Only letters
+  const validHexCode = /^#([0-9A-Fa-f]{6}|[0-9A-Fa-f]{3})$/; // Example: #123456 or #abc
+
+  if (validKeyword.test(input) || validHexCode.test(input)) {
+    return true; // Input is valid
+  }
+
+  return 'Invalid color. Enter a color keyword or a hexadecimal number.';
+}
+
 // Function to start the application
 async function start() {
   try {
-    // Use inquirer to collect user input
+    // Use inquirer to collect user input with color validation
     const userInput = await inquirer.prompt([
       {
         type: 'input',
         name: 'text',
         message: 'Enter up to three characters:',
-        validate: (input) => {
-          // Add validation logic for the text input
-        },
+        validate: validateText,
       },
       {
         type: 'input',
         name: 'textColor',
         message: 'Enter the text color (keyword or hex code):',
-        validate: (input) => {
-          // Add validation logic for the text color input
-        },
+        validate: validateColor,
       },
       {
         type: 'list',
@@ -33,9 +46,7 @@ async function start() {
         type: 'input',
         name: 'shapeColor',
         message: 'Enter the shape color (keyword or hex code):',
-        validate: (input) => {
-          // Add validation logic for the shape color input
-        },
+        validate: validateColor,
       },
     ]);
 
@@ -54,8 +65,9 @@ async function start() {
     }
     shape.setColor(userInput.shapeColor);
 
-    // Generate the SVG logo
-    const svg = `<svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">${shape.render()}</svg>`;
+    // Generate the SVG logo with the specified dimensions and shape
+const svg = `<svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">${shape.render()}</svg>`;
+
 
     // Write the SVG to a file
     fs.writeFileSync('logo.svg', svg);
